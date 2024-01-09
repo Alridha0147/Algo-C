2184b2a379d37a30614f69d26fe7b1f0d482f225
Alridha0147 <150901979+Alridha0147@users.noreply.github.com>
2024-01-09 15:29:46 +0000

    v0.0.1

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 20;
+    int num2 = 40;
+    float price = 10.99;
+    char c = 'c';
+    printf("Hello World %d \n", num2);
+
+    printf("la valeur de num = %d , la valeur de num2 = %i, prix = %.2f \n", num, num2, price, c);
+    return 0;
+}
