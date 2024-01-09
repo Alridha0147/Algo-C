f27ffe9ad10f0f67bb8f19e4aeb8bb0859ccbbaf
Alridha0147 <150901979+Alridha0147@users.noreply.github.com>
2024-01-09 15:44:43 +0000

    :v 0.0.5 : multiple variables declaration

--- main.c
+++ main.c
@@ -5,9 +5,22 @@ int main()
     int num = 20;
     int num2 = 40;
     float price = 10.99;
+
     char c = 'c';
+
+    int algo = 15, systeme = 16, sql = 19, merise = 15;
+
+    float moyenne; 
+    
     printf("Hello World %d \n", num2);
 
+
     printf("la valeur de num = %d , la valeur de num2 = %i, prix = %.2f \n", num, num2, price, c);
+    num = 30;
+    num2 = 40;
+    c = 'a';
+    
+    num = num2;
+    printf("new values : la valeur de num = %d , la valeur de num2 = %i, prix = %.2f, char value : %c \n", num, num2, price, c);
     return 0;
 }
