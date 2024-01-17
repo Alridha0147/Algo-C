f8d0e0cb507013b2d9f83adcadd32cfb2eaf44b7
Alridha0147 <150901979+Alridha0147@users.noreply.github.com>
2024-01-17 10:31:12 +0000

    v.0.0.3

--- main.c
+++ main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 
+void afficher_somme(int x)
+{
+    printf("La somme est de : %d\n", x);
+}
+
+
+int calcule_somme(int x, int y)
+{
+    return x + y;
+}
+
+void afficher_age(int age)
+{
+    if (age >= 18)
+    {
+        printf("Vous etes majeur\n");
+    }
+    else
+    {
+        printf("Vous n'etes pas majeur\n");
+    }
+}
+ 
 int main()
 {
+    int age = majeur(19); 
+    afficher_age(age);
+    return 0;
+}
+
+int main()
+{
+    int a = 6;
+    int b = 24;
+
+    afficher_somme(calcule_somme(6,24));
+    afficher_somme(calcule_somme(a,b));
+
+}
+
     int num = 20;
     int num2 = 40;
     float price = 10.99;
@@ -11,6 +49,9 @@ int main()
     int algo = 15, systeme = 16, sql = 19, merise = 15;
 
     float moyenne; 
+    int nbModule = 4;
+    int total = algo + systeme + merise;
+    moyenne = (float) total / nbModule;
     
     printf("Hello World %d \n", num2);
 
@@ -21,6 +62,9 @@ int main()
     c = 'a';
     
     num = num2;
-    printf("new values : la valeur de num = %d , la valeur de num2 = %i, prix = %.2f, char value : %c \n", num, num2, price, c);
+        printf("new values : la valeur de num = %d , la valeur de num2 = %i, prix = %.2f, char value : %c \n", num, num2, price, c);
+   
+    printf("new values : la valeur de num = %d , la valeur de num2 = %d ");
     return 0;
+
 }
