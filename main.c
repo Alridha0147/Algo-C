743b56798bf1f026bc385f9786295d03d732f404
Alridha0147 <150901979+Alridha0147@users.noreply.github.com>
2024-01-23 10:16:55 +0000

    :v 0.5.0 : add est_impaire function

--- main.c
+++ main.c
@@ -73,57 +73,12 @@ int le_plus_petit(int x, int y)
         return y;
 }
 
-int main()
+int est_impaire(int x)
 {
-    int age = majeur(19); 
-    afficher_age(age);
-    return 0;
+    return x % 2;
 }
-
-est_impaire(int x)
-{
- if (x % 2 == 1)
- return printf("TRUE");
-}
-
-
-
 
 int main()
 {
-    int a = 6;
-    int b = 24;
-
-    afficher_somme(calcule_somme(6,24));
-    afficher_somme(calcule_somme(a,b));
-
-}
-
-    int num = 20;
-    int num2 = 40;
-    float price = 10.99;
-
-    char c = 'c';
-
-    int algo = 15, systeme = 16, sql = 19, merise = 15;
-
-    float moyenne; 
-    int nbModule = 4;
-    int total = algo + systeme + merise;
-    moyenne = (float) total / nbModule;
     
-    printf("Hello World %d \n", num2);
-
-
-    printf("la valeur de num = %d , la valeur de num2 = %i, prix = %.2f \n", num, num2, price, c);
-    num = 30;
-    num2 = 40;
-    c = 'a';
-    
-    num = num2;
-        printf("new values : la valeur de num = %d , la valeur de num2 = %i, prix = %.2f, char value : %c \n", num, num2, price, c);
-   
-    printf("new values : la valeur de num = %d , la valeur de num2 = %d ");
-    return 0;
-
 }
